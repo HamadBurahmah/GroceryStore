c41ea8fd5f414045837f34be02cba151e1778ec0
Hamad Burahmah <hburahmah@o251-12.csuchico.edu>
2018-09-26 18:10:44 -0700

    changes

--- main.cpp
+++ main.cpp
@@ -4,16 +4,38 @@ using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        string item;
+        char input;
+        do{
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
 
-  return 0;
+
+
+
+                if(input == 'a'|| input == 'A'){
+
+                        cout<< "What is the item?"<<endl;
+                        cin >> item;
+                
+
+                if( numItems <5 ){
+
+                        list[numItems] == item;
+                        numItems++;
+                }
+                else{
+                        cout << "You'll need a bigger list!"<<endl;
+                }
+                }
+        }
+        while(input != 'q' && input!= 'Q');
+        return 0;
 }
