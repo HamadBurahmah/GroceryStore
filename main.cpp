9f4ef7e0901043879958ed24e9c8e285992df30e
Hamad Burahmah <hburahmah@o244-05.csuchico.edu>
2018-10-10 16:40:37 -0700

    done lab

--- main.cpp
+++ main.cpp
@@ -12,25 +12,32 @@ int main()
                 cout<<"\n==GROCERY LIST MANAGER==";
                 cout<<"\nEnter your choice: ";
                 cout<<"\n (A)dd an item";
+              if(list.size()>=1){
+                cout<<"\n (D)elete last item";
+              }
                 cout<<"\n (Q)uit";
                 cout<<"\nYour choice (A/Q): ";
                 cin>>input;
 
 
-
+           
 
 
                 if (input == 'a'|| input =='A'){
 
                         cout<< "What is the item?"<<endl;
-                        cin >> item;
-               
-
                 
-
-                        list.push_back(item);
-                       
+                        cin >> item;
+                list.push_back(item);
                 }
+    else if (input == 'd' || input == 'D'){
+                
+    cout << list[list.size()-1] << " was deleted from the list.\n";
+    list.pop_back();
+    }
+                       
+                       
+                
         }        while(input != 'q' && input!= 'Q');
         int num=1; 
         int index;
